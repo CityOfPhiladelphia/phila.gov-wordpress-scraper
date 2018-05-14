b2a1b6ae673d06d01ed3a7acba16450df48b3cdc
Andrew Madonna <andrew.w.madonna@gmail.com>
2018-05-14 17:08:30 -0400

    Threading

--- beta_static_generator.py
+++ beta_static_generator.py
@@ -1,23 +1,30 @@
 import os
 import sys
 import json
+import time
 import datetime
 import smtplib
 import logging
+import threading
 from logging.config import dictConfig
 from email.mime.text import MIMEText
+from datetime import datetime
+from queue import Queue
 
 import requests
 import boto3
 import click
 
-config = json.load(open('config.json'))
+with open('config.json') as file:
+    config = json.load(file)
 
 SAVE_FOLDER = config["save_folder"]
 NEW_URL = config["new_url"]
-HEADER = {'user-agent': 'beta-static-generator/0.0.1'}
-LASTRUN = config["last_run"]
 API_URL = config["api"]
+
+HEADER = {'user-agent': 'beta-static-generator/0.0.1'}
+
+THREAD_ERROR = False
 
 def init_logger(logging_config):
     try:
@@ -37,23 +44,27 @@ def init_logger(logging_config):
 
     return logger
 
-def save_page(url, save_s3, s3_client, s3_bucket):
+def save_page(logger, url, save_s3, s3_client, s3_bucket):
+    logger.info('Scraping: {}'.format(url))
     response = requests.get(url, headers = HEADER)
-    key = SAVE_FOLDER + url.replace("https://beta.phila.gov/", "")
+    key = SAVE_FOLDER + url.replace('https://beta.phila.gov/', '')
     if key.endswith('/'):
-        key = key + 'index.html'
-    __types = response.headers['Content-Type'].split(';')
-    __content_type = __types[0]
+        key += 'index.html'
+    content_type_list = response.headers['Content-Type'].split(';')
+    content_type = content_type_list[0]
 
-    if __content_type == 'text/html':
+    if content_type == 'text/html':
         body = response.text.replace('beta.phila.gov', NEW_URL)
     else:
         body = response.content
-    
+
     if save_s3:
-        s3_bucket.put_object(Key=key,
+        ## TODO: calculate md5 of body
+        ## TODO: check s3 head md5
+        s3_client.put_object(Bucket=s3_bucket,
+                             Key=key,
                              Body=body,
-                             ContentType=__content_type,
+                             ContentType=content_type,
                              ACL='public-read')
     else:
         path = os.path.join(os.getcwd(), key)
@@ -63,7 +74,13 @@ def save_page(url, save_s3, s3_client, s3_bucket):
             except OSError as exc: # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
-        with open(path, 'w') as file:
+
+        if content_type == 'text/html':
+            write_mode = 'w'
+        else:
+            write_mode = 'wb'
+
+        with open(path, write_mode) as file:
             file.write(body)
 
 def get_pages_list(url):
@@ -71,83 +88,114 @@ def get_pages_list(url):
     data = response.json()
     return data
 
-def send_error_notification(e):
+def send_error_email(orig_error, e):
     try:
         subject = "Beta generator encountered an error when building the site"
-        body = "Error details: " + str(e)
+        body = 'Original Error: {}\n\nSlack error details: {}'.format(orig_error, str(e))
         msg = MIMEText(subject + body)
         msg['Subject'] = subject
         msg['From'] = 'ithelp@phila.gov'
         msg['To'] = 'andrew.kennel@phila.gov'
         send_email(msg)
     except:
-        print("Fail")
+        logger.exception('Exception sending error email')
 
 def post_to_slack(message):
     post = {"text": "{0}".format(message)}
  
     try:
-        json_data = json.dumps(post)
-        requests.post("https://hooks.slack.com/services/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",
-                              data=json_data.encode('ascii'),
-                              headers={'Content-Type': 'application/json'})
+        requests.post('https://hooks.slack.com/services/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',
+                      json=post)
     except Exception as em:
         #Fall-back to sending an email
-        print(send_error_notification(em))
+        logger.exception('Exception sending error message to Slack')
+        send_error_email(message, em)
 
 def send_email(msg):
-    try:
-        s = smtplib.SMTP("relay.city.phila.local")
-        s.set_debuglevel(1)
-        s.send_message(msg)
-        s.quit()
-    except:
-        print("Fail")
+    s = smtplib.SMTP("relay.city.phila.local")
+    s.set_debuglevel(1)
+    s.send_message(msg)
+    s.quit()
+
+def stop_workers(q, threads):
+    for i in range(len(threads)):
+        q.put(None)
+    for t in threads:
+        t.join()
 
 @click.command()
 @click.option('--save-s3', is_flag=True, default=False, help='Save site to S3 bucket.')
 @click.option('--s3-bucket', default='static.merge.phila.gov', help='When saving to S3, the bucket to use.')
-@click.option('--logging-config', default='logging_config.conf')
-def main(save_s3, s3_bucket, logging_config):
+@click.option('--logging-config', default='logging_config.conf', help='Python logging config file in YAML format.')
+@click.option('--num-worker-threads', type=int, default=8, help='Number of workers.')
+@click.option('--notifications/--no-notifications', is_flag=True, default=False, help='Enable Slack/email error notifications.')
+def main(save_s3, s3_bucket, logging_config, num_worker_threads, notifications):
     logger = init_logger(logging_config)
-    s3_client = boto3.resource('s3')
-    s3_bucket = s3_client.Bucket('static.merge.phila.gov')
 
     logger.info('Starting scraper')
 
-    error = config["error"]
-    start_time = datetime.datetime.now()
+    q = Queue()
+    error_lock = threading.Lock()
+
+    def worker():
+        global THREAD_ERROR
+
+        try:
+            time.sleep(0.1) # Rate limits acquiring creds in so many threads
+            s3_client = boto3.resource('s3')
+        except Exception as e:
+            message = 'Exception creating S3 client in thread'
+            logger.exception(message)
+            error_lock.acquire()
+            THREAD_ERROR = message
+            error_lock.release()
+            raise e
+
+        while THREAD_ERROR is False:
+            url = q.get()
+            if url is None:
+                break
+            try:
+                save_page(logger, url, save_s3, s3_client, s3_bucket) ## TODO: try/catch
+            except Exception as e:
+                message = 'Exception scraping: {}'.format(url)
+                logger.exception(message)
+                error_lock.acquire()
+                THREAD_ERROR = message
+                error_lock.release()
+                raise e
+            q.task_done()
+
+    threads = []
+    for i in range(num_worker_threads):
+        t = threading.Thread(target=worker)
+        t.start()
+        threads.append(t)
+
     try:
-        # Only proceed if the last run was successful
-        # Forces an admin to manually clear the flag
-        # Hopefully after fixing whatever it is went wrong
-        if error == False:
-            api_query_url = API_URL + "?timestamp=" + LASTRUN
-            logger.info('Fetching page list from: {}'.format(api_query_url))
-            static_pages = open('staticfiles.csv','r').read().splitlines()
-            for page in static_pages:
-                logger.info('Saving static file {}'.format(page))
-                save_page(page, save_s3, s3_client, s3_bucket)
+        logger.info('Scraping static files')
+        static_pages = open('staticfiles.csv','r').read().splitlines()
+        for url in static_pages:
+            q.put(url)
 
-            page_data = get_pages_list(api_query_url)
-            for page in page_data:
-                url = page["link"]
-                logger.info('Scraping: {}'.format(url))
-                save_page(url, save_s3, s3_client, s3_bucket)
+        logger.info('Fetching page list from: {}'.format(API_URL))
+        page_data = get_pages_list(API_URL)
+        for page in page_data:
+            q.put(page["link"])
 
-            # finally set the LastRun value and save to the config file
-            config["LastRun"] = start_time.strftime("%Y-%m-%d %H:%M:%S")
-        else:
-            print("Error!")
-            #post_to_slack("There was an error scraping beta.phila.gov on the last run. Please clear the error.")
-    except:
-        error = True
+        while True:
+            if q.empty() or THREAD_ERROR is not False:
+                break
+            time.sleep(1)
+
+        stop_workers(q, threads)
+
+        if THREAD_ERROR is not False:
+            raise Exception(THREAD_ERROR)
+    except Exception as e:
         logger.exception('Exception occured scraping site')
-        #post_to_slack("Error scraping beta.phila.gov")
-    finally:
-        config["error"] = error
-        #with open('config.json', 'w') as outfile:
-        #    json.dump(config, outfile)
+        if notifications:
+            post_to_slack('Exception occured scraping site: ' + str(e))
 
 if __name__ == '__main__':
     main()
